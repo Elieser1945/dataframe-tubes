774a38011a0205777460f8b6323a16f293fc68a5
Elieser1945 <elieserpasaribu19@gmail.com>
2025-01-05 10:19:16 +0700

    dataframe_kelompok.py

--- dataframe1.py
+++ dataframe1.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
-data = {'Nama': ['Elieser Pasaribu'], 'NIM': [1103223209]}
+data = {
+    'Nama': ['Elieser Pasaribu', 'Najwa Syafira Firdaus'],
+    'NIM': ['1103223209', '1103223110']
+}
+
 df = pd.DataFrame(data)
 
 print(df)
